8bb260ca199b602429845fd2febf08bc0823817c
Juntao Wu <wujuntaocn@outlook.com>
2018-04-10 18:33:23 +0800

    init commit

---
+++ src/Main.ts
@@ -0,0 +1,305 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) 2014-present, Egret Technology.
+//  All rights reserved.
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Egret nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY EGRET AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+//  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//  IN NO EVENT SHALL EGRET AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS OF USE, DATA,
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+class Main extends eui.UILayer {
+
+    private customFilter: egret.CustomFilter;
+
+    constructor() {
+        super();
+        let fragmentSrc = [
+            "precision lowp float;\n" +
+            "varying vec2 vTextureCoord;",
+            "varying vec4 vColor;\n",
+            "uniform sampler2D uSampler;",
+
+            "uniform vec2 center;",
+            "uniform vec3 params;", // 10.0, 0.8, 0.1"
+            "uniform float time;",
+
+            "void main()",
+            "{",
+            "vec2 uv = vTextureCoord.xy;",
+            "vec2 texCoord = uv;",
+
+            "float dist = distance(uv, center);",
+
+            "if ( (dist <= (time + params.z)) && (dist >= (time - params.z)) )",
+            "{",
+            "float diff = (dist - time);",
+            "float powDiff = 1.0 - pow(abs(diff*params.x), params.y);",
+
+            "float diffTime = diff  * powDiff;",
+            "vec2 diffUV = normalize(uv - center);",
+            "texCoord = uv + (diffUV * diffTime);",
+            "}",
+
+            "gl_FragColor = texture2D(uSampler, texCoord);",
+            "}"
+        ].join("\n");
+
+        let vertexSrc =
+            "attribute vec2 aVertexPosition;\n" +
+            "attribute vec2 aTextureCoord;\n" +
+            "attribute vec2 aColor;\n" +
+
+            "uniform vec2 projectionVector;\n" +
+
+            "varying vec2 vTextureCoord;\n" +
+            "varying vec4 vColor;\n" +
+
+            "const vec2 center = vec2(-1.0, 1.0);\n" +
+
+            "void main(void) {\n" +
+            "   gl_Position = vec4( (aVertexPosition / projectionVector) + center , 0.0, 1.0);\n" +
+            "   vTextureCoord = aTextureCoord;\n" +
+            "   vColor = vec4(aColor.x, aColor.x, aColor.x, aColor.x);\n" +
+            "}";
+
+        this.customFilter = new egret.CustomFilter(vertexSrc, fragmentSrc, {
+            center: { x: 0, y: 0 },
+            params: { x: 10, y: 0.8, z: 0.1 },
+            time: 0
+        });
+    }
+
+    protected createChildren(): void {
+        super.createChildren();
+
+        egret.lifecycle.addLifecycleListener((context) => {
+            // custom lifecycle plugin
+        })
+
+        egret.lifecycle.onPause = () => {
+            egret.ticker.pause();
+        }
+
+        egret.lifecycle.onResume = () => {
+            egret.ticker.resume();
+        }
+
+        //inject the custom material parser
+        //注入自定义的素材解析器
+        let assetAdapter = new AssetAdapter();
+        egret.registerImplementation("eui.IAssetAdapter", assetAdapter);
+        egret.registerImplementation("eui.IThemeAdapter", new ThemeAdapter());
+
+
+        this.runGame().catch(e => {
+            console.log(e);
+        })
+    }
+
+    private async runGame() {
+        await this.loadResource()
+        this.createGameScene();
+        const result = await RES.getResAsync("description_json")
+        this.startAnimation(result);
+        await platform.login();
+        const userInfo = await platform.getUserInfo();
+        console.log(userInfo);
+        this.colorLabel.text = `Hello ${userInfo && userInfo.nickName}`;
+
+    }
+
+    private async loadResource() {
+        try {
+            const loadingView = new LoadingUI();
+            this.stage.addChild(loadingView);
+            await RES.loadConfig("resource/default.res.json", "resource/");
+            await this.loadTheme();
+            await RES.loadGroup("preload", 0, loadingView);
+            this.stage.removeChild(loadingView);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    private loadTheme() {
+        return new Promise((resolve, reject) => {
+            // load skin theme configuration file, you can manually modify the file. And replace the default skin.
+            //加载皮肤主题配置文件,可以手动修改这个文件。替换默认皮肤。
+            let theme = new eui.Theme("resource/default.thm.json", this.stage);
+            theme.addEventListener(eui.UIEvent.COMPLETE, () => {
+                resolve();
+            }, this);
+
+        })
+    }
+
+    private textfield: egret.TextField;
+    /**
+     * 创建场景界面
+     * Create scene interface
+     */
+    protected createGameScene(): void {
+        let sky = this.createBitmapByName("bg_jpg");
+        this.addChild(sky);
+        let stageW = this.stage.stageWidth;
+        let stageH = this.stage.stageHeight;
+        sky.width = stageW;
+        sky.height = stageH;
+
+        sky.filters = [this.customFilter];
+
+        this.addEventListener(egret.Event.ENTER_FRAME, this.onFilterRender, this);
+
+        let topMask = new egret.Shape();
+        topMask.graphics.beginFill(0x000000, 0.5);
+        topMask.graphics.drawRect(0, 0, stageW, 172);
+        topMask.graphics.endFill();
+        // topMask.y = 33; //consider if there need a status bar.
+        topMask.y = 0;
+        this.addChild(topMask);
+
+        // let icon: egret.Bitmap = this.createBitmapByName("egret_icon_png");
+        // this.addChild(icon);
+        // icon.x = 26;
+        // icon.y = 33;
+
+        // let line = new egret.Shape();
+        // line.graphics.lineStyle(2, 0xffffff);
+        // line.graphics.moveTo(0, 0);
+        // line.graphics.lineTo(0, 117);
+        // line.graphics.endFill();
+        // line.x = 172;
+        // line.y = 61;
+        // this.addChild(line);
+        
+        let colorLabel = new egret.TextField();
+        colorLabel.name = "colorLabel";
+        colorLabel.textColor = 0xffffff;
+        colorLabel.width = stageW;
+        colorLabel.textAlign = "center";
+        colorLabel.text = `Hello`;
+        colorLabel.size = 24;
+        colorLabel.x = 0;
+        colorLabel.y = 80;
+        this.addChild(colorLabel);
+        
+
+        let textfield = new egret.TextField();
+        this.addChild(textfield);
+        textfield.alpha = 0;
+        textfield.width = stageW;
+        textfield.textAlign = egret.HorizontalAlign.CENTER;
+        textfield.size = 24;
+        textfield.textColor = 0xffffff;
+        textfield.x = 0;
+        textfield.y = 135;
+        this.textfield = textfield;
+
+        let button = new eui.Button();
+        button.skinName = "ButtonStartSkin";
+        //button.skinName = "skin.ButtonStartSkin";
+        button.name = "button";
+        button.label = "Play Game";
+        button.horizontalCenter = 0;
+        button.verticalCenter = -100;
+        this.addChild(button);
+        button.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
+
+        let colorList = new ColorListComponent();
+        colorList.horizontalCenter = -30;
+        colorList.verticalCenter = -50;
+        this.addChild(colorList);
+        console.log("Main.ts this._homeUI:", colorList.width, colorList.height);
+    }
+    /**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
+    private createBitmapByName(name: string): egret.Bitmap {
+        let result = new egret.Bitmap();
+        let texture: egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        result.name = name;
+        return result;
+    }
+    /**
+     * 描述文件加载成功，开始播放动画
+     * Description file loading is successful, start to play the animation
+     */
+    private startAnimation(result: Array<any>): void {
+        let parser = new egret.HtmlTextParser();
+
+        let textflowArr = result.map(text => parser.parse(text));
+        let textfield = this.textfield;
+        let count = -1;
+        let change = () => {
+            count++;
+            if (count >= textflowArr.length) {
+                count = 0;
+            }
+            let textFlow = textflowArr[count];
+
+            // 切换描述内容
+            // Switch to described content
+            textfield.textFlow = textFlow;
+            let tw = egret.Tween.get(textfield);
+            tw.to({ "alpha": 1 }, 200);
+            tw.wait(2000);
+            tw.to({ "alpha": 0 }, 200);
+            tw.call(change, this);
+        };
+
+        change();
+    }
+
+    /**
+     * 点击按钮
+     * Click the button
+     */
+    private onButtonClick(e: egret.TouchEvent) {
+        this.button.filters = [this.customFilter];
+        let panel = new eui.Panel();
+        panel.title = "Title";
+        panel.horizontalCenter = 0;
+        panel.verticalCenter = 0;
+        this.addChild(panel);
+    }
+
+    private onFilterRender() {
+        this.customFilter.uniforms.time += 0.03;
+        if (this.customFilter.uniforms.time > 1) {
+            this.getChildByName("bg_jpg").filters = [];
+            //this.removeEventListener(egret.Event.ENTER_FRAME, eventHandler, this);
+            this.customFilter.uniforms.time = 0.0;
+        }
+    }
+
+    get button() {
+        return this.getChildByName("button");
+    }
+
+    get colorLabel() {
+        return this.getChildByName("colorLabel") as egret.TextField;
+    }
+}
